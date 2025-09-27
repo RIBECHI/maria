32f87d8408aec54bdd11702185a880eeec026b6f
AGRORIBECHI <agroribechi@gmail.com>
2025-09-27 15:55:59 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  "projectId": "lexmanager",
+  "appId": "1:487471917143:web:82033f194252d4ada95b12",
+  "apiKey": "AIzaSyAgRM15QQg0Kf0BMFhP7V-GmN5aPF_Z3zk",
+  "authDomain": "lexmanager.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "487471917143"
 };
 
 // Initialize Firebase
