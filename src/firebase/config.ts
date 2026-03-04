5ed2ad2657963d49b1196364b4d11cc618e837f8
desafyo <desafyo@gmail.com>
2026-03-04 02:56:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "lexmanager",
+  "appId": "1:487471917143:web:82033f194252d4ada95b12",
+  "apiKey": "AIzaSyAgRM15QQg0Kf0BMFhP7V-GmN5aPF_Z3zk",
+  "authDomain": "lexmanager.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "487471917143"
+};
